5393d28137f4d9aed6a60665935eb3c0718225f8
BlackCoffeee <arief.veriez@gmail.com>
2025-01-03 00:43:49 +0800

    Add AuthModule for user authentication management
    - Created AuthModule to encapsulate authentication logic.
    - Integrated JwtModule for JWT token management with configurable secret and expiration.
    - Registered AuthService and JwtStrategy for handling authentication processes.
    - Included AuthController for managing authentication-related endpoints.
    - This addition lays the foundation for a robust authentication system in the application.

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthService } from './auth.service';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { AuthController } from './auth.controller';
+import { UserModule } from '../user/user.module';
+import { envConfig } from '../config/env.config';
+import { JWT_CONSTANTS } from './constants/auth.constant';
+
+@Module({
+    imports: [
+        UserModule,
+        JwtModule.register({
+            secret: envConfig.jwt.secret,
+            signOptions: { 
+                expiresIn: JWT_CONSTANTS.ACCESS_TOKEN_EXPIRATION 
+            },
+        }),
+    ],
+    providers: [AuthService, JwtStrategy],
+    controllers: [AuthController],
+    exports: [AuthService],
+})
+export class AuthModule {}
