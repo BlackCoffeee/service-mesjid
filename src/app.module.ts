8541d8496b5bfe8b0028a427bc9eb87c2d8bc1ac
BlackCoffeee <arief.veriez@gmail.com>
2025-01-03 00:40:44 +0800

    Integrate AppConfigModule into AppModule for configuration management

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { CommonModule } from './common/common.module';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { AppConfigModule } from './config/config.module';
 
 @Module({
-    imports: [CommonModule, UserModule, AuthModule],
+    imports: [
+        AppConfigModule,
+        CommonModule,
+        UserModule,
+        AuthModule,
+    ],
     controllers: [],
     providers: [],
 })
