dc1e7edf9476f2c91e6506ed605e887dd71a0ef9
BlackCoffeee <arief.veriez@gmail.com>
2025-01-05 12:02:49 +0800

    Refactor module imports to use path aliases for improved clarity
    - Updated import statements in `app.module.ts` to utilize path mappings defined in `tsconfig.json`.
    - Changed module paths from relative to absolute using the configured aliases for better organization and maintainability.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
-import { CommonModule } from './common/common.module';
-import { UserModule } from './user/user.module';
-import { AuthModule } from './auth/auth.module';
-import { AppConfigModule } from './config/config.module';
+import { CommonModule } from '@common/common.module';
+import { UserModule } from '@modules/user/user.module';
+import { AuthModule } from '@modules/auth/auth.module';
+import { AppConfigModule } from '@config/config.module';
 
 @Module({
     imports: [AppConfigModule, CommonModule, UserModule, AuthModule],
